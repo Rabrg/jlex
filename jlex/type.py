8cd4de2fc4c459554295c55e5c1e728010ec895c
Ryan Greene <rabrg96@gmail.com>
2018-02-18 01:35:28 -0600

    Removed Type todos

--- jlex/type.py
+++ jlex/type.py
@@ -1,8 +1,6 @@
 from enum import Enum
 
 
-# TODO: ternary, four space indention, streams, operators
-# https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html
 class Type(Enum):
     BLOCK_COMMENT = "(/\\*.*?\\*/).*"
     LINE_COMMENT = "(//(.*?)[\r$]?\n).*"
