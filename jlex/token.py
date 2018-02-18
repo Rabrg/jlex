31195d09382e97d5ecefeeb217078922365d63a8
Ryan Greene <rabrg96@gmail.com>
2018-02-17 20:15:35 -0600

    Generalized token string method

--- jlex/token.py
+++ jlex/token.py
@@ -1,6 +1,3 @@
-from jlex.type import Type
-
-
 class Token:
 
     def __init__(self, t, v):
@@ -8,8 +5,6 @@ class Token:
         self.v = v
 
     def __str__(self):
-        if self.t == Type.atom:
-            return 'ATOM<' + self.v + '>'
-        return self.v
+        return self.t.name + '<' + self.v + '>'
 
     __repr__ = __str__
