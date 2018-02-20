bd6e19577d023ed05d08a45fbfc489b5792aee39
Ryan Greene <rabrg96@gmail.com>
2018-02-20 12:15:24 -0600

    Refactored input -> string in lexer.py

--- jlex/lexer.py
+++ jlex/lexer.py
@@ -10,29 +10,29 @@ class LexicalError(Exception):
         self.message = message
 
 
-def lex(input):
+def lex(string):
     tokens = list()
     index = 0
-    while index < len(input):
-        token = separate_token(input, index)
+    while index < len(string):
+        token = separate_token(string, index)
         if token is None:
             break
         index = token.end
         tokens.append(token)
-    if index != len(input):
+    if index != len(string):
         raise LexicalError('Lexical error at position ' + str(index))
     return tokens
 
 
-def separate_token(input, begin):
-    if begin < 0 or begin >= len(input):
-        raise IndexError(input, 'Index out of bounds: ' + begin)
+def separate_token(string, begin):
+    if begin < 0 or begin >= len(string):
+        raise IndexError(string, 'Index out of bounds: ' + begin)
     for type in Type:
         pattern = r'.{' + str(begin) + '}' + type.value
-        match = re.match(pattern, input, re.DOTALL)
+        match = re.match(pattern, string, re.DOTALL)
         if match:
             end = match.end(1)
             if type == Type.STRING_LITERAL or type == Type.CHARACTER_LITERAL:
                 end += 1
-            return Token(begin, end, input[begin:end], type)
+            return Token(begin, end, string[begin:end], type)
     return None
